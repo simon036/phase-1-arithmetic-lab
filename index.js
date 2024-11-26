7b2990dccea19dc11370a708d863b4f8c5301126
Simon <ndungusimon564@gmail.com>
2024-11-26 19:04:17 +0300

    arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31; 
+let num2 = 2; 
+let num3 = 24; 
+let num4 = 20; 
+
+let multiply = num1 * num2; 
+let random = Math.floor(Math.random() * 100) + 1; 
+
+let mod = num3 % num4; 
+
+
+let max = Math.max(10, 15, 20, 5); 
